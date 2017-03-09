2639556f7f372b7d4a471596fa49525cc99c0b44
Admindo <Admindo@WorkStation>
2017-03-09 05:06:48 +0000

    test

---
+++ rentcar/src/main/java/org/sympanux/RentcarApplication.java
@@ -0,0 +1,13 @@
+package org.sympanux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentcarApplication.class, args);
+		System.out.println("hello");
+	}
+}
